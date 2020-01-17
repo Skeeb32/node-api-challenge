d77ddebcf5ec0e51b96f9367872bceeaba6872cf
Shaqib Habib <55864051+Skeeb32@users.noreply.github.com>
2020-01-17 10:01:33 -0800

    Actions Projects

---
+++ actions.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const dbActions = require("./data/helpers/actionModel");
+const router = express.Router();
+
+//Get
+router.get('/', async (req, res) => {
+    try {
+      const actions = await dbActions.get()
+      res.status(200).json(actions)
+    }
+    catch{
+      res.status(500).json({ error: "error" })
+    }
+  
+  });
+
+  //Post
+
+router.post("/:id", async (req, res) => {
+    const newPost = {...req.body, project_id: req.params.id };
+    try {
+        const success = await dbActions.insert(newPost);
+        res.status(201).json(success);
+    } catch {
+        res.status(500).json({error: "Invalid ID specified"});
+    }
+})
+
+//Put
+router.put('/:id', async (req, res) => {
+    try {
+      await dbActions.update(req.params.id, { ...req.body, id: req.params.id })
+      const newResult = await dbActions.getById(req.params.id)
+      res.status(200).json(newResult)
+    }
+    catch {
+      res.status(500).json({ error: "500 Error" })
+    }
+  });
+
+  // Delete
+  router.delete('/:id', async (req, res) => {
+    try {
+      const result = await dbActions.remove(req.params.id)
+      res.status(200).json({ status: `User Id: ${result} has been successfully deleted` })
+    }
+    catch {
+      res.status(500).json({ error: "500 Error" })
+    }
+  });
+
+
+
+module.exports = router;
---
+++ projects.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const dbProjects = require("./data/helpers/projectModel");
+const router = express.Router();
+
+//Get
+router.get('/', async (req, res) => {
+    try {
+      const projects = await dbProjects.get()
+      res.status(200).json(projects)
+    }
+    catch{
+      res.status(500).json({ error: "error" })
+    }
+  
+  });
+
+  //Post
+
+router.post("/", async (req, res) => {
+    const newPost = {...req.body };
+    try {
+        const success = await dbProjects.insert(newPost);
+        res.status(201).json(success);
+    } catch {
+        res.status(500).json({error: "Invalid data specified"});
+    }
+})
+
+//Put
+router.put('/:id', async (req, res) => {
+    try {
+      await dbProjects.update(req.params.id, { ...req.body, id: req.params.id })
+      const newResult = await dbProjects.getById(req.params.id)
+      res.status(200).json(newResult)
+    }
+    catch {
+      res.status(500).json({ error: "500 Error" })
+    }
+  });
+
+  // Delete
+  router.delete('/:id', async (req, res) => {
+    try {
+      const result = await dbProjects.remove(req.params.id)
+      res.status(200).json({ status: `User Id: ${result} has been successfully deleted` })
+    }
+    catch {
+      res.status(500).json({ error: "500 Error" })
+    }
+  });
+
+
+module.exports = router;
